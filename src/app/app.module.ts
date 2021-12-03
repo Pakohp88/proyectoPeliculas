281b1beb1c5d3628f945e0e7a9c3a4c2036eb798
Francisco Hernandez Perez <franciscohernandez@lavore.com.mx>
2021-12-02 19:58:05 -0600

    Se agregan los componentes y los estilos de bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SingupComponent } from './components/singup/singup.component';
+import { LoginComponent } from './components/login/login.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingupComponent,
+    LoginComponent,
+    PeliculasComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
